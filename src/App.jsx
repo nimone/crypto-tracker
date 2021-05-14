c28a4967bc6dc0b21271db1f98d1eacf965efc78
nimone <nishantmogha1234@gmail.com>
2021-05-14 11:14:46 +0530

    coins are rendering and search is working

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
+import Coin from './components/Coin'
 
 function App() {
   const [coins, setCoins] = useState([])
+  const [search, setSearch] = useState('')
 
   useEffect(() => {
     fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
@@ -14,9 +16,38 @@ function App() {
       .catch(err => console.log(err))
   }, [])
 
+  const handleSearch = e => {
+    e.preventDefault
+    setSearch(e.target.value)
+  }
+
+  const filteredCoins = coins.filter(coin => 
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  )
+
   return (
     <div className="App">
       <h1>Crypto Price Tracker</h1>
+      <div className="coin-search">
+        <input 
+          type="text" 
+          placeholder="Search" 
+          className="coin-input"
+          onChange={handleSearch}
+        />
+      </div>
+      <div className="coins">
+        {filteredCoins.map(coin => (
+          <Coin 
+            key={coin.id}
+            name={coin.name}
+            image={coin.image}
+            symbol={coin.symbol}
+            price={coin.current_price}
+            volume={coin.total_volume}
+          />
+        ))}
+      </div>
     </div>
   )
 }
